8082cc2950865ace9083e198a7de9c0620057114
siriaou <saouradker@gmail.com>
2025-04-24 11:18:43 -0500

    uploaded question10 png

---
+++ Sireesha/Day2/question_10.sql
@@ -0,0 +1,18 @@
+/* 10) Sample Northwind database:
+Download
+I.	 Download northwind.sql from below link into your local. Sign in to Git first https://github.com/pthom/northwind_psql
+II.	 Manually Create the database using pgAdmin:
+A.	 Right-click on "Databases" → Create → Database
+B.	Give name as ‘northwind’ (all small letters)
+C.	Click ‘Save’
+
+Import database:
+I.	 Open pgAdmin and connect to your server          	
+II.	  Select the database  ‘northwind’
+III.	  Right Click-> Query tool.
+IV.	  Click the folder icon to open your northwind.sql file
+V.	 Press F5 or click the Execute button.
+VI.	  You will see total 14 tables loaded
+VII.	  Databases → your database → Schemas → public → Tables*/
+
+/* Followed above steps and completed the assignment . Created Northwind database. Attached screenshot */
