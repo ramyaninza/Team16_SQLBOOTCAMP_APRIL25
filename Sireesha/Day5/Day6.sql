884d599dd3976cfffbaa2d6529e06aad5db504c0
siriaou <saouradker@gmail.com>
2025-04-29 22:42:55 -0500

    Da6 assignments are done

---
+++ Sireesha/Day5/Day6.sql
@@ -0,0 +1,113 @@
+SELECT * FROM order_details
+/*1.      Categorize products by stock status
+(Display product_name, a new column stock_status whose values are based on below condition
+ units_in_stock = 0  is 'Out of Stock'
+       units_in_stock < 20  is 'Low Stock')*/
+
+Select product_name,
+units_in_stock,
+    CASE
+    WHEN units_in_stock = 0 THEN 'Out of stock'
+    WHEN units_in_stock < 20 THEN 'Low stock'
+    ELSE 'In stock'
+  END AS stock_status
+FROM
+    products;
+
+/*Day 6,2, 2.      Find All Products in Beverages Category
+(Subquery, Display product_name,unitprice)*/
+
+SELECT Product_name,
+unit_price
+FROM 
+products
+WHERE 
+category_id = (
+SELECT category_id
+FROM categories
+WHERE category_name = 'Beverages'
+)
+
+/*DAY 6,3.      Find Orders by Employee with Most Sales
+(Display order_id,   order_date,  freight, employee_id.
+Employee with Most Sales=Get the total no.of of orders for each employee then
+order by DESC and limit 1. Use Subquery)*/
+
+SELECT order_id,
+order_date,
+freight,
+employee_id
+FROM orders
+WHERE employee_id = (
+SELECT employee_id
+FROM orders
+Group By employee_id
+ORDER BY COUNT (*) DESC
+LIMIT 1
+)
+/*4.      Find orders  where for country!= ‘USA’ with freight costs higher
+than any order from USA. (Subquery, Try with ANY, ALL operators)*/
+
+SELECT 
+    order_id,
+    customer_id,
+    ship_country,
+    freight
+FROM 
+    orders
+WHERE 
+    ship_country != 'USA'
+    AND freight > ANY (
+        SELECT freight 
+        FROM orders 
+        WHERE ship_country = 'USA'
+    )
+
+ /*****with ALL****/
+
+ SELECT 
+    order_id,
+    customer_id,
+    ship_country,
+    freight
+FROM 
+    orders
+WHERE 
+    ship_country != 'USA'
+    AND freight > ALL (
+        SELECT freight 
+        FROM orders 
+        WHERE ship_country = 'USA'
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
