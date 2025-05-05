5078d246cd55c5158d6cd7153d84938ba259f8a2
siriaou <saouradker@gmail.com>
2025-05-04 20:20:02 -0500

    updated Day9 assignments
    updated Day9 assignments

--- Day9/Sireesha_Day9_Assignments.sql
+++ Day9/Sireesha_Day9_Assignments.sql
@@ -1,1 +1,91 @@
-/*Day 9 assignments*/
+--Day 9,1.1.Create AFTER UPDATE trigger to track product price changes
+
+ 
+·       Create product_price_audit table with below columns:
+	audit_id SERIAL PRIMARY KEY,
+    product_id INT,
+    product_name VARCHAR(40),
+    old_price DECIMAL(10,2),
+    new_price DECIMAL(10,2),
+    change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_name VARCHAR(50) DEFAULT CURRENT_USER
+ 
+·       Create a trigger function with the below logic:
+ 
+  INSERT INTO product_price_audit (
+        product_id,
+        product_name,
+        old_price,
+        new_price
+    )
+    VALUES (
+        OLD.product_id,
+        OLD.product_name,
+        OLD.unit_price,
+        NEW.unit_price
+    );
+·       Create a row level trigger for below event:
+          	AFTER UPDATE OF unit_price ON products
+ 
+·        Test the trigger by updating the product price by 10% to any one product_id.
+
+CREATE TABLE product_price_audit (
+    audit_id SERIAL PRIMARY KEY,
+    product_id INT,
+    product_name VARCHAR(40),
+    old_price DECIMAL(10,2),
+    new_price DECIMAL(10,2),
+    change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_name VARCHAR(50) DEFAULT CURRENT_USER
+);
+CREATE OR REPLACE FUNCTION log_price_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO product_price_audit (
+        product_id,
+        product_name,
+        old_price,
+        new_price
+    )
+    VALUES (
+        OLD.product_id,
+        OLD.product_name,
+        OLD.unit_price,
+        NEW.unit_price
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER trg_product_price_change
+AFTER UPDATE OF unit_price ON products
+FOR EACH ROW
+WHEN (OLD.unit_price IS DISTINCT FROM NEW.unit_price)
+EXECUTE FUNCTION log_price_change();
+
+
+-- Check existing price
+SELECT product_id, product_name, unit_price FROM products WHERE product_id = 1;
+
+-- Update price by 10%
+UPDATE products
+SET unit_price = unit_price * 1.1
+WHERE product_id = 1;
+
+-- Check audit table
+SELECT * FROM product_price_audit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
